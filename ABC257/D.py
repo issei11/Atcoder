ce24b226857299b5945d6b41eac1e4ee4ebcac05
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2022-08-16 12:42:49 +0900

    Modified ABC257

--- ABC257/D.py
+++ ABC257/D.py
@@ -57,3 +57,33 @@ while True:
         print(s_need[m][0])
         break
     m += 1
+
+
+"class UnionFind:",
+"\tdef __init__(self, n):",
+"\t\tself.parent = [i for i in range(n)]",
+"\t\tself.height = [0 for _ in range(n)] # 各⽊の⾼さ",
+"\t",
+"\tdef get_root(self, i):",
+"\t\tif self.parent[i] == i: # ⾃分が根ノードの場合",
+"\t\t\treturn i",
+"\t\telse: # 経路圧縮しながら根ノードを探す",
+"\t\t\tself.parent[i] = self.get_root(self.parent[i])",
+"\t\t\treturn self.parent[i]",
+"\t",
+"\tdef unite(self, i, j):",
+"\t\troot_i = self.get_root(i)",
+"\t\troot_j = self.get_root(j)",
+"\t\tif root_i != root_j: # より⾼い⽅にマージ",
+"\t\t\tif self.height[root_i] < self.height[root_j]:",
+"\t\t\t\tself.parent[root_i] = root_j",
+"\t\t\telse:",
+"\t\t\t\tself.parent[root_j] = root_i",
+"\t\t\t\tif self.height[root_i] == self.height[root_j]:",
+"\t\t\t\t\tself.height[root_i] += 1",
+"\t",
+"\tdef is_in_group(self, i, j):",
+"\t\tif self.get_root(i) == self.get_root(j):",
+"\t\t\treturn True",
+"\t\telse:",
+"\t\t\treturn False"
